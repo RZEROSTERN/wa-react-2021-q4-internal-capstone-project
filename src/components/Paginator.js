45f88e88a7a23bb43ddf459e8044503db87de16e
Marco Ramirez <marco.ramirez@rzerocorp.com>
2021-11-21 20:09:59 -0600

    Fixed class to className on Paginator

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -1,19 +1,19 @@
 function Paginator() {
     return (
-        <div class="pagination wz-right">
-            <ul class="pagination-2">
-            <li class="page-number prev"><a href="/#">&laquo;</a></li>
-            <li class="page-number"><a href="/#">1</a></li>
-            <li class="page-number active"><a href="/#">2</a></li>
-            <li class="page-number"><a href="/#">3</a></li>
-            <li class="page-number"><a href="/#">4</a></li>
-            <li class="page-number"><a href="/#">5</a></li>
-            <li class="page-number"><a href="/#">6</a></li>
-            <li class="page-number"><a href="/#">7</a></li>
-            <li class="page-number"><a href="/#">8</a></li>
-            <li class="page-number"><a href="/#">9</a></li>
-            <li class="page-number"><a href="/#">10</a></li>
-            <li class="page-number prev"><a href="/#">&raquo;</a></li>
+        <div className="pagination wz-right">
+            <ul className="pagination-2">
+                <li className="page-number prev"><a href="/#">&laquo;</a></li>
+                <li className="page-number"><a href="/#">1</a></li>
+                <li className="page-number active"><a href="/#">2</a></li>
+                <li className="page-number"><a href="/#">3</a></li>
+                <li className="page-number"><a href="/#">4</a></li>
+                <li className="page-number"><a href="/#">5</a></li>
+                <li className="page-number"><a href="/#">6</a></li>
+                <li className="page-number"><a href="/#">7</a></li>
+                <li className="page-number"><a href="/#">8</a></li>
+                <li className="page-number"><a href="/#">9</a></li>
+                <li className="page-number"><a href="/#">10</a></li>
+                <li className="page-number prev"><a href="/#">&raquo;</a></li>
             </ul>
         </div>
     )
