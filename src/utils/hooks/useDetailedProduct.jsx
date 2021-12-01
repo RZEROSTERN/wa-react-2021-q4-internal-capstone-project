740171d09adc698bab7662befd413f14b1203873
Marco Ramirez <marco.ramirez@rzerocorp.com>
2021-12-01 15:39:40 -0600

    Finished hook for detailed product

--- src/utils/hooks/useDetailedProduct.jsx
+++ src/utils/hooks/useDetailedProduct.jsx
@@ -1,10 +1,47 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useLatestAPI } from "./useLatestAPI"
+import { API_BASE_URL } from "../constants"
 
-export function useDetailedProduct() {
+export function useDetailedProduct(productID) {
     const { ref: apiRef, isLoading: isApiMetadataLoading } = useLatestAPI()
-    const [categories, setCategories] = useState(() => ({
+    const [detailedProduct, setDetailedProduct] = useState(() => ({
         data: {},
         isLoading: true
     }))
+
+    useEffect(() => {
+        if (!apiRef || isApiMetadataLoading || productID === null) {
+            return () => {};
+        }
+
+        const controller = new AbortController()
+
+        async function getDetailedProduct() {
+            try {
+                setDetailedProduct({ data: {}, isLoading: true })
+
+                const response = await fetch(
+                    `${API_BASE_URL}/documents/search?ref=${apiRef}&q=${encodeURIComponent(
+                        '[[at(document.id, "' + productID + '")]]'
+                    )}&lang=en-us&pageSize=1`, {
+                        signal: controller.signal,
+                    }
+                );
+
+                const data = await response.json()
+                setDetailedProduct( {data, isLoading: false} )
+            } catch(err) {
+                setDetailedProduct({data: {}, isLoading: false})
+                console.error(err)
+            }
+        }
+
+        getDetailedProduct()
+
+        return () => {
+            controller.abort()
+        }
+    }, [apiRef, isApiMetadataLoading, productID])
+
+    return detailedProduct
 }
--- src/views/ProductDetail.jsx
+++ src/views/ProductDetail.jsx
@@ -1,10 +1,26 @@
 import { useParams } from 'react-router-dom';
+import { useDetailedProduct } from '../utils/hooks/useDetailedProduct';
 
 const ProductDetail = () => {
     const {id} = useParams()
+    const {data, isLoading} = useDetailedProduct(id)
+    console.log(data, isLoading)
+    const product = (!isLoading) ? data.results[0] : null
 
-    return(
-        <h1>Product detail {id}</h1>
+    return (product !== null) ? (
+        <div className="wz-container">
+            <div>
+                <h1>{product.data.name}</h1>
+                <div className="wz-halfs"></div>
+                <div className="wz-halfs">
+                    <p>{product.data.description}</p>
+                </div>
+            </div>
+        </div>
+    ) : (
+        <div className="wz-container">
+            <h1>Please wait...</h1>
+        </div>  
     )
 }
 
